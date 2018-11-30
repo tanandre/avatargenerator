c6cb9f2312d18414b60fdec833d7fbf5c2c5f66c
tanandre <tanneman1@gmail.com>
2018-11-29 21:37:27 -0500

    initial commit

---
+++ src/components/Avatar.vue
@@ -0,0 +1,11 @@
+<template>
+    <svg width="200" height="200" class="svg">
+      <!-- head shape -->
+      <path d="M50 140 C 50 180, 150 180, 150 140 V100 C 150 80, 50 80, 50 100Z" stroke="black" stroke-width="4" fill="transparent"/>
+    </svg>
+</template>
+
+<script>
+export default {
+};
+</script>
