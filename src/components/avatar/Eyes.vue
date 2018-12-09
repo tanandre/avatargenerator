1ddb8a7a12e447d236afde64ba31e7e2b0015899
tanandre <tanneman1@gmail.com>
2018-12-08 22:50:14 -0500

    add iris colors

--- src/components/avatar/Eyes.vue
+++ src/components/avatar/Eyes.vue
@@ -1,10 +1,22 @@
 <template>
   <g>
     <defs>
-      <linearGradient id="eyeBrown" gradientTransform="rotate(90)">
+      <radialGradient id="iris00">
+        <stop offset="40%" stop-color="gold"/>
+        <stop offset="85%" stop-color="green"/>
+      </radialGradient>
+        <radialGradient id="iris01">
+        <stop offset="40%" stop-color="#9ee3f9"/>
+        <stop offset="85%" stop-color="#0f76ff"/>
+      </radialGradient>
+      <linearGradient id="iris02" gradientTransform="rotate(90)">
           <stop offset="20%" stop-color="#000"/>
           <stop offset="50%" stop-color="#C55010"/>
-        </linearGradient>
+      </linearGradient>
+      <linearGradient id="eyeWhite" gradientTransform="rotate(80)">
+        <stop offset="45%" style="stop-color:rgb(0,0,0);stop-opacity:0.8" />
+        <stop offset="50%" style="stop-color:rgb(0,0,0);stop-opacity:0" />
+      </linearGradient>
       <path id="eyeOutline" :transform="`rotate(${eyes.rotate.value}, 0, 0)`"
             :d="`M ${eyes.outerWidth.value / 2} 0
           c -${eyes.outerWidth.value / 2} ${eyes.outerHeight.value}, -${eyes.outerWidth.value / 2} ${eyes.outerHeight.value}, -${eyes.outerWidth.value} 0
@@ -12,33 +24,44 @@
           Z`"
             stroke="black"
             stroke-width="3"
+
           ></path>
+          <path id="eyeOutlineTop" :transform="`rotate(${eyes.rotate.value}, 0, 0)`"
+            :d="`M -${eyes.outerWidth.value / 2} 0
+          c ${eyes.outerWidth.value / 4} -${eyes.outerHeight.value} ${eyes.outerWidth.value - eyes.outerWidth.value / 4} -${eyes.outerHeight.value} ${eyes.outerWidth.value} 0
+          `"
+          ></path>
+      <path id="eyeOutlineBottom" :transform="`rotate(${eyes.rotate.value}, 0, 0)`"
+            :d="`M ${eyes.outerWidth.value / 2} 0
+          c -${eyes.outerWidth.value / 2} ${eyes.outerHeight.value}, -${eyes.outerWidth.value / 2} ${eyes.outerHeight.value}, -${eyes.outerWidth.value} 0
+          `"
+            stroke="black"
+            stroke-width="4"
+          ></path>
+      <g id="eyeBall">
+        <circle x="0" y="0" r="50" fill="#fff"/>
+        <!-- <circle x="0" y="0" r="50" fill="url(#eyeWhite)"/> -->
+        <ellipse  stroke="black"
+            stroke-width="3" :cx="0" :cy="0" :rx="eyes.innerWidth.value / 2" :ry="eyes.innerWidth.value / 2"
+             :fill="`url(#iris0${eyes.iris.value})`"></ellipse>
+        <circle :cx="0" :cy="0" r="6" fill="black" />
+        <ellipse :cx="5" :cy="-4" rx="2" ry="3" fill="white"></ellipse>
+      </g>
       <clipPath id="eyeMaskLeft">
         <use href="#eyeOutline" />
       </clipPath>
       <clipPath id="eyeMaskRight">
         <use href="#eyeOutline" transform="scale(-1, 1)"/>
       </clipPath>
-      <g id="eyeBall">
-        <circle x="0" y="0" r="50" fill="#fff"/>
-        <ellipse :cx="0" :cy="0" :rx="eyes.innerWidth.value / 2" :ry="eyes.innerWidth.value / 2" fill="brown"></ellipse>
-        <ellipse :cx="0" :cy="0" :rx="eyes.innerWidth.value / 2 - eyes.innerWidth.value / 8" :ry="eyes.innerWidth.value / 2 - eyes.innerWidth.value / 8" fill="#000"></ellipse>
-        <ellipse :cx="5" :cy="-3" rx="2" ry="2" fill="white"></ellipse>
-      </g>
-      <g id="eyeBallDeep">
-        <circle x="0" y="0" r="50" fill="#fff"/>
-        <ellipse  stroke="black"
-            stroke-width="3" :cx="0" :cy="0" :rx="eyes.innerWidth.value / 2" :ry="eyes.innerWidth.value / 2"
-             fill="url(#eyeBrown)"></ellipse>
-        <!-- <ellipse :cx="0" :cy="0" :rx="5" :ry="eyes.innerWidth.value / 2 - eyes.innerWidth.value / 8" fill="#000"></ellipse> -->
-        <circle :cx="0" :cy="0" r="6" fill="black" />
-        <ellipse :cx="5" :cy="-4" rx="2" ry="3" fill="white"></ellipse>
-      </g>
     </defs>>
-    <!-- <use :x="startXLeft" :y="startY" xlink:href="#eyeOutline" /> -->
-    <use :x="startXLeft"  :y="startY" xlink:href="#eyeBallDeep" style="clip-path: url(#eyeMaskLeft);" />
-    <!-- <use :x="-startXRight" :y="startY" xlink:href="#eyeOutline" transform="scale(-1, 1)" /> -->
-    <use :x="startXRight" :y="startY" xlink:href="#eyeBallDeep" style="clip-path: url(#eyeMaskRight);" />
+    <g>
+      <use :x="startXLeft" :y="startY" xlink:href="#eyeOutlineTop" stroke="black" stroke-width="8" />
+      <use :x="startXLeft" :y="startY" xlink:href="#eyeBall" style="clip-path: url(#eyeMaskLeft);" />
+    </g>
+      <g>
+      <use :x="-startXRight" :y="startY" xlink:href="#eyeOutlineTop" stroke="black" stroke-width="8" transform="scale(-1, 1)" />
+      <use :x="startXRight" :y="startY" xlink:href="#eyeBall" style="clip-path: url(#eyeMaskRight);" />
+    </g>
   </g>
 </template>
 
