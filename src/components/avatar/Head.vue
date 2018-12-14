8b319a3354b74da664f26232d80530ba8a7bdcad
tanandre <tanneman1@gmail.com>
2018-12-14 16:59:08 -0500

    add head color depth

--- src/components/avatar/Head.vue
+++ src/components/avatar/Head.vue
@@ -1,38 +1,36 @@
+<template>
+  <g>
+    <defs>
+      <g id="head">
+        <path :d="`m-${head.width.value / 2} -${head.height.value / 2} v${head.height.value}
+          c ${-head.chinWidth.value} ${head.chin.value}, ${head.width.value + head.chinWidth.value} ${head.chin.value}, ${head.width.value} 0
+          v-${head.height.value}
+          c 0 -100, -${head.width.value} -100, -${head.width.value} 0Z`"/>
+       </g>
+      <g id="headSmaller" transform="scale(0.9, 0.98)">
+        <path :d="`m-${head.width.value / 2} -${head.height.value / 2} v${head.height.value}
+          c ${-head.chinWidth.value} ${head.chin.value}, ${head.width.value + head.chinWidth.value} ${head.chin.value}, ${head.width.value} 0
+          v-${head.height.value}
+          c 0 -100, -${head.width.value} -100, -${head.width.value} 0Z`"/>
+      </g>
+  </defs>
+    <use :x="`${startX}`" :y="`${startY}`" :xlink:href="`#head`" :fill="`${head.color}`" stroke="black" stroke-width="3"/>
+    <use :x="`${startX}`" :y="`${startY}`" :xlink:href="`#head`" fill="rgba(0,0,0,0.2)"/>
+    <use :x="`${startX}`" :y="`${startY}`"  :xlink:href="`#headSmaller`" :fill="`${head.color}`"/>
+</g>
+</template>
 <script>
 export default {
   computed: {
+    head() {
+      return this.$store.state.face.head;
+    },
     startX() {
-      return (
-        (this.$store.state.frame.dimensions.width
-          - this.$store.state.face.head.width.value)
-        / 2
-      );
+      return this.$store.state.frame.dimensions.width / 2;
     },
     startY() {
-      return (
-        (this.$store.state.frame.dimensions.height
-          - this.$store.state.face.head.height.value)
-          / 2
-        + this.$store.state.face.head.offsetY.value
-      );
+      return this.$store.state.frame.dimensions.height / 2;
     },
-  },
-  render(h) {
-    const { head } = this.$store.state.face;
-
-    return h('g', [
-      h('path', {
-        attrs: {
-          d: `m${this.startX} ${this.startY} v${head.height.value}
-          c ${-head.chinWidth.value} ${head.chin.value}, ${head.width.value + head.chinWidth.value} ${head.chin.value}, ${head.width.value} 0 
-          v-${head.height.value}
-          c 0 -100, -${head.width.value} -100, -${head.width.value} 0Z`,
-          stroke: 'black',
-          'stroke-width': '3',
-          fill: head.color,
-        },
-      }),
-    ]);
   },
 };
 </script>
