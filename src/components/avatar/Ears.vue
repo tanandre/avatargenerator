7e26970cfe1d3ada41bd486a2c90195836bef1bb
tanandre <tanneman1@gmail.com>
2018-12-15 15:22:13 -0500

    change ears to use template

--- src/components/avatar/Ears.vue
+++ src/components/avatar/Ears.vue
@@ -1,49 +1,43 @@
+<template>
+  <g>
+    <defs>
+      <g id="ear">
+        <path :d="`M -${head.width.value / 2 - wide - 10} 0
+          h-${wide}
+          c -${ears.width.value / 2} 0,
+            -${ears.width.value} -${ears.height.value}
+            0 -${ears.height.value}`"/>
+      </g>
+    </defs>
+    <use :x="`${startX}`" :y="`${startY}`" :xlink:href="`#ear`" stroke="black" stroke-width="2"/>
+    <use :x="`-${startX}`" :y="`${startY}`" transform="scale(-1, 1)" :xlink:href="`#ear`" stroke="black" stroke-width="2"/>
+  </g>
+</template>
 <script>
 export default {
+  data() {
+    return {
+      wide: 20,
+    };
+  },
   computed: {
+    head() {
+      return this.$store.state.face.head;
+    },
+    ears() {
+      return this.$store.state.face.ears;
+    },
     startX() {
-      return (
-        (this.$store.state.frame.dimensions.width
-          - this.$store.state.face.head.width.value)
-        / 2
-      );
+      return this.$store.state.frame.dimensions.width / 2;
     },
     startY() {
       return (
         (this.$store.state.frame.dimensions.height
           - this.$store.state.face.head.height.value + this.$store.state.face.ears.height.value)
           / 2
-        + this.$store.state.face.head.offsetY.value + this.$store.state.face.ears.offsetY.value + 60
+        + this.$store.state.face.head.offsetY.value + this.$store.state.face.ears.offsetY.value
       );
     },
-  },
-  render(h) {
-    const { ears } = this.$store.state.face;
-    const wide = 10;
-
-    return h('g', [
-      h('path', {
-        attrs: {
-          d: `M ${this.startX + wide} ${this.startY} 
-          h-${wide}
-          c -${ears.width.value / 2} 0, 
-            -${ears.width.value} -${ears.height.value}, 
-            0 -${ears.height.value}`,
-          stroke: 'black',
-          'stroke-width': '3',
-        },
-      }), h('path', {
-        attrs: {
-          d: `M ${this.startX - wide + this.$store.state.face.head.width.value} ${this.startY} 
-          h${wide}
-          c ${ears.width.value / 2} 0, 
-            ${ears.width.value} -${ears.height.value}, 
-            0 -${ears.height.value}`,
-          stroke: 'black',
-          'stroke-width': '3',
-        },
-      }),
-    ]);
   },
 };
 </script>
