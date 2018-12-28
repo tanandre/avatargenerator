bb7dba748528f914f8144d0f64a8f2ac26119e62
tanandre <tanneman1@gmail.com>
2018-12-28 10:23:02 -0500

    rewrite nose shadow

--- src/components/avatar/Nose.vue
+++ src/components/avatar/Nose.vue
@@ -1,18 +1,25 @@
 <template>
   <g>
     <defs>
+      <g id="nose00">
+        <path :d="`
+        M0 ${nose.height.value - 7}
+        c ${nose.width.value / 3} 0 ${nose.width.value / 3 * 2} -5 ${nose.width.value} 0
+        s -${nose.width.value / 3 * 2} 15 ${-nose.width.value} 15
+        M0 ${nose.height.value - 7}
+        c -${nose.width.value / 3} 0 -${nose.width.value / 3 * 2} -5 ${-nose.width.value} 0
+        s ${nose.width.value / 3 * 2} 15 ${nose.width.value} 15
+        `" fill="rgb(0,0,0, 0.2)" stroke="none"/>
+        <!-- <path :d="`M-0 ${nose.height.value - 5}
+        c ${nose.width.value / 2} -10 ${nose.width.value + nose.width.value / 2} -10 ${nose.width.value * 2} 0
+        c -${nose.width.value / 2} 10 -${nose.width.value + nose.width.value / 2} 10 -${nose.width.value * 2} 0`" fill="rgb(0,0,0, 0.1)" stroke="none"/> -->
+        <path :d="`M${nose.width.value / 2 - 2} ${nose.height.value - 4} c 0 -2 5 -2 ${nose.width.value / 2} 0`"/>
+        <path :d="`M-${nose.width.value / 2 - 2} ${nose.height.value - 4} c 0 -2 -5 -2 -${nose.width.value / 2} 0`"/>
+      </g>
       <g id="nose01">
         <path :d="`M0 0 c 0 10 5 ${nose.height.value - 10} 0 ${nose.height.value}`"/>
         <path :d="`M${nose.width.value / 4} ${nose.height.value - 4} c 0 -2 5 -2 ${nose.width.value / 4} 0`"/>
         <path :d="`M-${nose.width.value / 4 + 2} ${nose.height.value - 4} c 0 -2 -5 -2 -${nose.width.value / 4} 0`"/>
-      </g>
-      <g id="nose00">
-        <path :d="`M-${nose.width.value} ${nose.height.value - 5}
-        c ${nose.width.value / 2} -10 ${nose.width.value + nose.width.value / 2} -10 ${nose.width.value * 2} 0
-        c -${nose.width.value / 2} 10 -${nose.width.value + nose.width.value / 2} 10 -${nose.width.value * 2} 0`" fill="rgb(0,0,0, 0.1)" stroke="none"/>
-
-        <path :d="`M${nose.width.value / 2 - 2} ${nose.height.value - 4} c 0 -2 5 -2 ${nose.width.value / 2} 0`"/>
-        <path :d="`M-${nose.width.value / 2 - 2} ${nose.height.value - 4} c 0 -2 -5 -2 -${nose.width.value / 2} 0`"/>
       </g>
     </defs>
     <use :x="`${startX}`" :y="`${startY}`" :xlink:href="`#nose0${nose.type.value}`" stroke="black" stroke-width="2"/>
