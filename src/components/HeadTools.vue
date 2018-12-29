9d8121dd1f6bf842b6727b9a24335ad57fb4f5bc
tanandre <tanneman1@gmail.com>
2018-12-29 11:11:15 -0500

    fix js error

--- src/components/HeadTools.vue
+++ src/components/HeadTools.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <v-card v-for="(obj, objName) in $store.state.face" :key="objName">
+    <v-card v-for="(obj, objName) in $store.state.face" :key="objName" v-if="typeof $store.state.face[objName] === 'object'">
       <v-card-text>
         <h3>{{objName}}</h3>
-        <div class="item" v-if="typeof $store.state.face[objName] === 'object'" v-for="(item, itemName) in obj" :key="itemName">
-        <InputNumber
-          v-if="$store.state.face[objName][itemName].value !== undefined"
-            v-model="$store.state.face[objName][itemName].value"
-            :label="itemName"
-            :max="$store.state.face[objName][itemName].max($store.state.face)"
-            :min="$store.state.face[objName][itemName].min($store.state.face)"
-          />
-          <input v-else-if="typeof $store.state.face[objName][itemName] === 'string'" v-model="$store.state.face[objName][itemName]"/>
-          <input type="checkbox" v-else-if="typeof $store.state.face[objName][itemName] === 'boolean'" v-model="$store.state.face[objName][itemName]"/>
-          <div v-else class="warning">{{item}}</div>
-        </div>
+          <div class="item" v-for="(item, itemName) in obj" :key="itemName">
+          <InputNumber
+            v-if="$store.state.face[objName][itemName].value !== undefined"
+              v-model="$store.state.face[objName][itemName].value"
+              :label="itemName"
+              :max="$store.state.face[objName][itemName].max($store.state.face)"
+              :min="$store.state.face[objName][itemName].min($store.state.face)"
+            />
+            <input v-else-if="typeof $store.state.face[objName][itemName] === 'string'" v-model="$store.state.face[objName][itemName]"/>
+            <input type="checkbox" v-else-if="typeof $store.state.face[objName][itemName] === 'boolean'" v-model="$store.state.face[objName][itemName]"/>
+            <div v-else class="warning">{{item}}</div>
+          </div>
       </v-card-text>
     </v-card>
   </div>
