7bc592908e9360d285a2e85879e650b7a978acb8
tanandre <tanneman1@gmail.com>
2018-12-29 11:13:58 -0500

    remove unused code

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,33 +1,5 @@
 <template>
     <svg :width="$store.state.frame.dimensions.width" :height="$store.state.frame.dimensions.height" class="svg" :fill="$store.state.face.head.color">
-      <defs>
-<clipPath id="leftEyeClip">
-          <rect x="2" y="2" width="100" height="40" />
-      </clipPath>
-<clipPath id="leftEyeClip2">
-<path  d="M 180 215
-        c -25 10, -25 10, -50 0
-        c 12.5 -10 37.5 -10 50 0
-        Z" stroke="black" stroke-width="2"></path>
-</clipPath>
-        <mask id="mask2" x="0" y="0" width="50" height="50" >
-      <circle cx="15" cy="20" r="100" style="stroke:none; fill: #ffffff"/>
-    </mask>
-        <!-- <linearGradient id="eyeWhite">
-          <stop offset="2%" stop-color="#aaa"/>
-          <stop offset="20%" stop-color="#fff"/>
-          <stop offset="80%" stop-color="#fff"/>
-          <stop offset="95%" stop-color="#aaa"/>
-        </linearGradient>
-
-        <pattern id="eyePattern" x="0" y="0" width="25" height="25">
-          <rect x="0" y="0" width="50" height="50" fill="url(#eyeWhite)"/>
-          <circle cx="25" cy="5" r="10" fill="black" stroke="brown" stroke-width="4"/>
-          <circle cx="32" cy="5" r="2" fill="white" />
-        </pattern> -->
-      </defs>
-
-
       <CustomPath :path="$store.state.face.customPath2"/>
       <HairBack v-if="$store.state.face.hair.show"/>
       <Shirt :position="0" :color1="$store.state.face.shirt.color1" :color2="$store.state.face.shirt.color2"/>
