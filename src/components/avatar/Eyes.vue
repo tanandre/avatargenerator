56193f3bc08fa48bdff52ce867af1644f7effdde
tanandre <tanneman1@gmail.com>
2018-12-29 22:00:40 -0500

    improve whiskers

--- src/components/avatar/Eyes.vue
+++ src/components/avatar/Eyes.vue
@@ -17,18 +17,38 @@
         <stop offset="45%" style="stop-color:rgb(0,0,0);stop-opacity:0.8" />
         <stop offset="50%" style="stop-color:rgb(0,0,0);stop-opacity:0" />
       </linearGradient>
-      <path id="eyeOutline" :transform="`rotate(${eyes.rotate.value}, 0, 0)`"
+      <path id="eyeOutline00" :transform="`rotate(${eyes.rotate.value}, 0, 0)`"
             :d="`M ${eyes.outerWidth.value / 2} 0
           c -${eyes.outerWidth.value / 2} ${eyes.outerHeight.value}, -${eyes.outerWidth.value / 2} ${eyes.outerHeight.value}, -${eyes.outerWidth.value} 0
           c ${eyes.outerWidth.value / 4} -${eyes.outerHeight.value} ${eyes.outerWidth.value - eyes.outerWidth.value / 4} -${eyes.outerHeight.value} ${eyes.outerWidth.value} 0
           Z`"
             stroke="black"
             stroke-width="3"
+          ></path>
+      <path id="eyeOutline01" :transform="`rotate(${eyes.rotate.value}, 0, 0)`"
+            :d="`M ${eyes.outerWidth.value / 2} -${eyes.outerHeight.value / 2}
+c-15 -${eyes.outerHeight.value / 5 * 2} -${eyes.outerWidth.value - 5} -${eyes.outerHeight.value / 5 * 2} -${eyes.outerWidth.value} ${eyes.outerHeight.value / 5 * 2}
+c 5 ${eyes.outerHeight.value / 5 * 2} 8 ${eyes.outerHeight.value / 5 * 3} ${eyes.outerWidth.value / 5} ${eyes.outerHeight.value / 5 * 3}
+c10 2 ${eyes.outerWidth.value / 2} 2 ${eyes.outerWidth.value / 5 * 3} 0
+c 0 0 8 -${eyes.outerHeight.value / 5 * 2} ${eyes.outerWidth.value / 5} -${eyes.outerHeight.value}
+`"
+            stroke="black"
+            stroke-width="3"
 
           ></path>
-          <path id="eyeOutlineTop" :transform="`rotate(${eyes.rotate.value}, 0, 0)`"
+      <path id="eyeOutlineTop01" :transform="`rotate(${eyes.rotate.value}, 0, 0)`"
+            :d="`M ${eyes.outerWidth.value / 2} -${eyes.outerHeight.value / 2}
+c-15 -${eyes.outerHeight.value / 5 * 2} -${eyes.outerWidth.value - 5} -${eyes.outerHeight.value / 5 * 2} -${eyes.outerWidth.value} ${eyes.outerHeight.value / 5 * 2}
+c 5 ${eyes.outerHeight.value / 5 * 2} 8 ${eyes.outerHeight.value / 5 * 3} ${eyes.outerWidth.value / 5} ${eyes.outerHeight.value / 5 * 3}
+c 0 0 0 0 -${eyes.outerWidth.value / 5 - 5} -${eyes.outerHeight.value / 5 * 3}
+c 0 -10 50 -10 ${eyes.outerWidth.value} -${eyes.outerHeight.value / 5 * 2}
+
+`"
+          ></path>
+      <path id="eyeOutlineTop00" :transform="`rotate(${eyes.rotate.value}, 0, 0)`"
             :d="`M -${eyes.outerWidth.value / 2} 0
-          c ${eyes.outerWidth.value / 4} -${eyes.outerHeight.value} ${eyes.outerWidth.value - eyes.outerWidth.value / 4} -${eyes.outerHeight.value} ${eyes.outerWidth.value} 0
+          c ${eyes.outerWidth.value / 4} -${eyes.outerHeight.value} ${eyes.outerWidth.value - eyes.outerWidth.value / 4} -${eyes.outerHeight.value} ${eyes.outerWidth.value + 0} 0
+          c -${eyes.outerWidth.value / 4 - 5} -${eyes.outerHeight.value + whiskerHeight} -${eyes.outerWidth.value - eyes.outerWidth.value / 4 + 5} -${eyes.outerHeight.value + whiskerHeight} -${eyes.outerWidth.value+ 0} 1
           `"
           ></path>
       <g id="eyeBall00">
@@ -48,19 +68,19 @@
         <ellipse :cx="5" :cy="-4" rx="2" ry="3" fill="white"></ellipse>
       </g>
       <clipPath id="eyeMaskLeft">
-        <use href="#eyeOutline" />
+        <use href="#eyeOutline00" />
       </clipPath>
       <clipPath id="eyeMaskRight">
-        <use href="#eyeOutline" transform="scale(-1, 1)"/>
+        <use href="#eyeOutline00" transform="scale(-1, 1)"/>
       </clipPath>
     </defs>>
     <g>
-      <use :x="startXLeft" :y="startY" xlink:href="#eyeOutlineTop" stroke="black" stroke-width="8" />
       <use :x="startXLeft" :y="startY" :xlink:href="`#eyeBall0${eyes.irisType.value}`" style="clip-path: url(#eyeMaskLeft);" />
+      <use :x="startXLeft" :y="startY" xlink:href="#eyeOutlineTop00" stroke="black" stroke-width="1" fill="#000" />
     </g>
       <g>
-      <use :x="-startXRight" :y="startY" xlink:href="#eyeOutlineTop" stroke="black" stroke-width="8" transform="scale(-1, 1)" />
       <use :x="startXRight" :y="startY" :xlink:href="`#eyeBall0${eyes.irisType.value}`" style="clip-path: url(#eyeMaskRight);" />
+      <use :x="-startXRight" :y="startY" xlink:href="#eyeOutlineTop00" stroke="black" stroke-width="1" transform="scale(-1, 1)" fill="#000"/>
     </g>
   </g>
 </template>
@@ -68,6 +88,11 @@
 <script>
 
 export default {
+  data() {
+    return {
+      whiskerHeight: 5,
+    };
+  },
   computed: {
     eyes() {
       return this.$store.state.face.eyes;
@@ -95,10 +120,9 @@ export default {
   },
 };
 /**
-
-m170 160
-c 0 80 150 50 100 170
-c 150 -250 -100 -300 -100 -170
+m200 150
+c25 -10 85 -10 100 20
+c-20 50 -20 40 -90 35
 
  */
 </script>
