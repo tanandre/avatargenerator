4ee5a1afb32bc7db53c036921f6ded6ec799a904
tanandre <tanneman1@gmail.com>
2019-01-03 21:38:26 -0500

    extract AvatarSandbox to prep for standalone

---
+++ src/components/Avatar.vue
@@ -0,0 +1,47 @@
+<template>
+  <g :fill="face.head.color" >
+    <title>Avatar</title>
+    <HairBack v-if="face.hair.show" :frame="frame" :face="face"/>
+    <Shirt :position="0" :color1="face.shirt.color1" :color2="face.shirt.color2" :frame="frame" :face="face"/>
+    <Neck :frame="frame" :face="face"/>
+    <Shirt :position="1" :color1="face.shirt.color1" :color2="face.shirt.color2" :frame="frame" :face="face"/>
+    <Ears v-if="face.ears.show" :frame="frame" :face="face"/>
+    <Head v-if="face.head.show" :frame="frame" :face="face"/>
+    <Eyes v-if="face.eyes.show" :frame="frame" :face="face"/>
+    <Brows v-if="face.brows.show" :frame="frame" :face="face"/>
+    <Nose v-if="face.nose.show" :frame="frame" :face="face"/>
+    <Mouth v-if="face.mouth.show" :frame="frame" :face="face"/>
+    <Hair v-if="face.hair.show" :frame="frame" :face="face"/>
+  </g>
+</template>
+<script>
+
+import Head from '@/components/avatar/Head.vue';
+import Eyes from '@/components/avatar/Eyes.vue';
+import Mouth from '@/components/avatar/Mouth.vue';
+import Nose from '@/components/avatar/Nose.vue';
+import Hair from '@/components/avatar/Hair.vue';
+import HairBack from '@/components/avatar/HairBack.vue';
+import Ears from '@/components/avatar/Ears.vue';
+import Brows from '@/components/avatar/Brows.vue';
+import Neck from '@/components/avatar/Neck.vue';
+import Shirt from '@/components/avatar/Shirt.vue';
+
+export default {
+  props: ['face', 'frame'],
+  components: {
+    Head,
+    Brows,
+    Eyes,
+    Mouth,
+    Nose,
+    Hair,
+    HairBack,
+    Ears,
+    Neck,
+    Shirt,
+  },
+};
+</script>
+<style>
+</style>
--- src/components/AvatarSandbox.vue
+++ src/components/AvatarSandbox.vue
@@ -1,58 +1,23 @@
 <template>
-    <svg :width="$store.state.frame.dimensions.width" :height="$store.state.frame.dimensions.height" class="svg" :fill="face.head.color" id="avatarSvg">
+    <svg :width="$store.state.frame.dimensions.width" :height="$store.state.frame.dimensions.height" class="svg" id="avatarSvg">
       <CustomPath :path="$store.state.customPath2"/>
-      <g>
-        <title>Avatar</title>
-        <HairBack v-if="face.hair.show" :frame="frame" :face="face"/>
-        <Shirt :position="0" :color1="face.shirt.color1" :color2="face.shirt.color2" :frame="frame" :face="face"/>
-        <Neck :frame="frame" :face="face"/>
-        <Shirt :position="1" :color1="face.shirt.color1" :color2="face.shirt.color2" :frame="frame" :face="face"/>
-        <Ears v-if="face.ears.show" :frame="frame" :face="face"/>
-        <Head v-if="face.head.show" :frame="frame" :face="face"/>
-        <Eyes v-if="face.eyes.show" :frame="frame" :face="face"/>
-        <Brows v-if="face.brows.show" :frame="frame" :face="face"/>
-        <Nose v-if="face.nose.show" :frame="frame" :face="face"/>
-        <Mouth v-if="face.mouth.show" :frame="frame" :face="face"/>
-        <Hair v-if="face.hair.show" :frame="frame" :face="face"/>
-      </g>
+      <Avatar :frame="frame" :face="face" />
       <CustomPath :path="$store.state.customPath1"/>
       <CustomPath v-if="$store.state.help.grid" :path="this.faceGrid"/>
     </svg>
 </template>
-
 <script>
 
-import Head from '@/components/avatar/Head.vue';
-import Eyes from '@/components/avatar/Eyes.vue';
-import Mouth from '@/components/avatar/Mouth.vue';
-import Nose from '@/components/avatar/Nose.vue';
-import Hair from '@/components/avatar/Hair.vue';
-import HairBack from '@/components/avatar/HairBack.vue';
-import Ears from '@/components/avatar/Ears.vue';
-import Brows from '@/components/avatar/Brows.vue';
-import Neck from '@/components/avatar/Neck.vue';
-import Shirt from '@/components/avatar/Shirt.vue';
+import Avatar from './Avatar.vue';
 import CustomPath from '@/components/avatar/CustomPath.vue';
 
 export default {
   props: ['face', 'frameWidth', 'frameHeight'],
   components: {
-    Head,
-    Brows,
-    Eyes,
-    Mouth,
-    Nose,
-    Hair,
-    HairBack,
-    Ears,
-    Neck,
-    Shirt,
+    Avatar,
     CustomPath,
   },
   computed: {
-    // face() {
-    //   return generate(this.id);
-    // },
     faceGrid() {
       return `
       m200 ${this.frame.dimensions.height / 2 + this.head.height.value / 3 * 2}
@@ -72,12 +37,6 @@ export default {
     },
     frame() {
       return this.$store.state.frame;
-    },
-    startX() {
-      return (this.frame.dimensions.width - this.face.head.width) / 2;
-    },
-    startY() {
-      return (this.frame.dimensions.height - this.face.head.height) / 2 + this.face.head.offsetY;
     },
   },
 };
