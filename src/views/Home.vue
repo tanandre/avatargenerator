a2c2943dbc3c0988f80192c25fa84e02455aff8d
Andre Tan <andre.tan@forcare.com>
2021-10-31 14:36:42 -0400

    turn off custom paths

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,8 +18,8 @@
 
   <div>
     <AvatarSandbox :face="$store.state.face" :width="$store.state.frame.dimensions.width" :height="$store.state.frame.dimensions.height" />
-    <v-textarea outline class="customTA" v-model="$store.state.customPath1" label="front" placeholder="custom svg path" @keydown.stop />
-    <v-textarea outline class="customTA" v-model="$store.state.customPath2" label="back" placeholder="custom svg path" @keydown.stop />
+    <!-- <v-textarea outline class="customTA" v-model="$store.state.customPath1" label="front" placeholder="custom svg path" @keydown.stop />
+    <v-textarea outline class="customTA" v-model="$store.state.customPath2" label="back" placeholder="custom svg path" @keydown.stop /> -->
   </div>
       </v-container>
     </v-content>
